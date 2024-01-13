550f98a98cf7a294815ae66ccb82eb103fce7d12
danilo775 <danilo.uba@hotmail.com>
2024-01-13 00:05:38 -0300

    exercicio 01 Pratica

--- PraticaP006/src/leituraArquivo/LeituraDeArquivo.java
+++ PraticaP006/src/leituraArquivo/LeituraDeArquivo.java
@@ -1,5 +1,28 @@
 package leituraArquivo;
 
-public class LeituraDeArquivo {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
+public class LeituraDeArquivo {
+	public static void main(String[] args) {
+     
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("entrada.txt"));
+
+            System.out.println("Conteúdo do arquivo:");
+
+            String linha;
+            while ((linha = reader.readLine()) != null) {
+                System.out.println(linha);
+            }
+
+            reader.close();
+        } catch (IOException e) {
+            System.err.println("Erro ao ler o arquivo: " + e.getMessage());
+        } finally {
+        	
+        }
+    }
 }

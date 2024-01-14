502ee995ba21c0c46ac9b816901f7e5ba40435d3
danilo775 <danilo.uba@hotmail.com>
2024-01-14 00:27:10 -0300

    copia de arquivo em Java

--- PraticaP006/src/Arquivo/EscritaDeArquivo.java
+++ PraticaP006/src/Arquivo/EscritaDeArquivo.java
@@ -1,4 +1,4 @@
-package leituraArquivo;
+package Arquivo;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
--- PraticaP006/src/Arquivo/LeituraDeArquivo.java
+++ PraticaP006/src/Arquivo/LeituraDeArquivo.java
@@ -1,4 +1,4 @@
-package leituraArquivo;
+package Arquivo;
 
 import java.io.BufferedReader;
 import java.io.FileReader;

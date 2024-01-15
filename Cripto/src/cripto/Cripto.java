dbbfce17169555398bc0fea6c7fc679d6778b45a
danilo775 <danilo.uba@hotmail.com>
2024-01-15 11:56:57 -0300

    atv copia criptografada exemplo 2 slide Aquivo Binario

--- Cripto/src/cripto/Cripto.java
+++ Cripto/src/cripto/Cripto.java
@@ -26,6 +26,48 @@
 //	}
 //}
 
+////////////////////////////////////////////////////////
+//exemplo 3
+///////////////////////////////////////////////////////
+
+
+//package cripto;
+//
+//import java.io.FileInputStream;
+//import java.io.FileNotFoundException;
+//import java.io.FileOutputStream;
+//import java.io.IOException;
+//
+//public class Cripto {
+//    public static void process(String file1, String file2, String senha) throws FileNotFoundException {
+//        FileOutputStream saida = new FileOutputStream(file2);
+//
+//        FileInputStream entrada = new FileInputStream(file1);
+//        try {
+//            boolean eof = false;
+//            int count = 0;
+//            while (!eof) {
+//                int input = entrada.read();
+//                if (input != -1) {
+//                    // Operação XOR com 255
+//                    byte byteCriptografado = (byte) (input ^ 255);
+//                    saida.write(byteCriptografado);
+//                    count++;
+//                } else
+//                    eof = true;
+//            }
+//            entrada.close();
+//            System.out.println("\nBytes lidos: " + count);
+//        } catch (IOException e) {
+//            System.out.println("Erro -- " + e.toString());
+//        }
+//    }
+//}
+
+////////////////////////////////////////////////////////
+//exemplo 3
+///////////////////////////////////////////////////////
+
 package cripto;
 
 import java.io.FileInputStream;
@@ -34,27 +76,33 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class Cripto {
-    public static void process(String file1, String file2, String senha) throws FileNotFoundException {
-        FileOutputStream saida = new FileOutputStream(file2);
+public static void process(String file1, String file2, String senha) throws FileNotFoundException {
+FileOutputStream saida = new FileOutputStream(file2);
 
-        FileInputStream entrada = new FileInputStream(file1);
-        try {
-            boolean eof = false;
-            int count = 0;
-            while (!eof) {
-                int input = entrada.read();
-                if (input != -1) {
-                    // Operação XOR com 255
-                    byte byteCriptografado = (byte) (input ^ 255);
-                    saida.write(byteCriptografado);
-                    count++;
-                } else
-                    eof = true;
-            }
-            entrada.close();
-            System.out.println("\nBytes lidos: " + count);
-        } catch (IOException e) {
-            System.out.println("Erro -- " + e.toString());
-        }
-    }
+FileInputStream entrada = new FileInputStream(file1);
+try {
+boolean eof = false;
+
+
+int count = 0, contSenha = 0;
+while (!eof) {
+int input = entrada.read();
+if(contSenha >= senha.length()) {
+contSenha=0;
 }
+if (input != -1) {
+// Operação XOR com 255
+byte byteCriptografado = (byte) (input ^ senha.charAt(contSenha));
+saida.write(byteCriptografado);
+count++;
+} else
+eof = true;
+}
+entrada.close();
+System.out.println("\nBytes lidos: " + count);
+} catch (IOException e) {
+System.out.println("Erro -- " + e.toString());
+}
+}
+}
+

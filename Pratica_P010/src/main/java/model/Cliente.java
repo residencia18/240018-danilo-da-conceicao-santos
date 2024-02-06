e3c70fa3865f88077866dc8a9d38046de0ae2ac4
danilo775 <danilo.uba@hotmail.com>
2024-02-06 12:10:18 -0300

    p010

---
+++ Pratica_P010/src/main/java/model/Cliente.java
@@ -0,0 +1,56 @@
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Cliente {
+	@Id
+	@GeneratedValue (strategy=GenerationType.AUTO)
+	private Integer id;
+	private String nome;
+	private String cpf;
+
+	
+	public Cliente(Integer id, String nome, String cpf) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.cpf = cpf;
+	}
+	
+	public Cliente() {
+		super();
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [id=" + id + ", nome=" + nome + ", cpf=" + cpf + "]";
+	}
+	
+	
+	
+	
+}

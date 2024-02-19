b5528bfc883cc43c5080381166b0b6db7d8ac05f
danilo775 <danilo.uba@hotmail.com>
2024-02-19 13:55:31 -0300

    spring boot H2

---
+++ Aviacao/src/main/java/com/aviacao/airdegas/Aviacao/repository/AeroportoRepository.java
@@ -0,0 +1,9 @@
+package com.aviacao.airdegas.Aviacao.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.aviacao.airdegas.Aviacao.model.Aeroporto;
+
+public interface AeroportoRepository extends JpaRepository<Aeroporto, Long> { 
+
+}
---
+++ Aviacao/src/main/java/com/aviacao/airdegas/Aviacao/repository/ModeloAeronaveRepository.java
@@ -0,0 +1,9 @@
+package com.aviacao.airdegas.Aviacao.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.aviacao.airdegas.Aviacao.model.ModeloAeronave;
+
+public interface ModeloAeronaveRepository extends JpaRepository<ModeloAeronave, Long> {
+
+}

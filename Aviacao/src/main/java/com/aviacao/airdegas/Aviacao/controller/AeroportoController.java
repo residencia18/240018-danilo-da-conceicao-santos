01f44fde86f77cb3eef6a7405d3302175ab0e4f7
danilo775 <danilo.uba@hotmail.com>
2024-02-20 12:01:28 -0300

    aviaçao

--- Aviacao/src/main/java/com/aviacao/airdegas/Aviacao/controller/AeroportoController.java
+++ Aviacao/src/main/java/com/aviacao/airdegas/Aviacao/controller/AeroportoController.java
@@ -1,5 +1,21 @@
 package com.aviacao.airdegas.Aviacao.controller;
 
-public class AeroportoController {
+import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.aviacao.airdegas.Aviacao.model.Aeroporto;
+import com.aviacao.airdegas.Aviacao.repository.AeroportoRepository;
+
+@RestController
+public class AeroportoController {
+	@Autowired
+	private AeroportoRepository aeroportoRepository;
+	
+	@GetMapping("/aeroportos")
+	public ArrayList<Aeroporto> listarAeroportos() {
+		return (ArrayList<Aeroporto>) aeroportoRepository.findAll();
+	}
 }
--- Aviacao/src/main/java/com/aviacao/airdegas/Aviacao/controller/ModeloAeronaveController.java
+++ Aviacao/src/main/java/com/aviacao/airdegas/Aviacao/controller/ModeloAeronaveController.java
@@ -1,5 +1,22 @@
 package com.aviacao.airdegas.Aviacao.controller;
 
-public class ModeloAeronaveController {
+import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.aviacao.airdegas.Aviacao.model.ModeloAeronave;
+import com.aviacao.airdegas.Aviacao.repository.ModeloAeronaveRepository;
+
+@RestController
+public class ModeloAeronaveController {
+	@Autowired
+	private ModeloAeronaveRepository modeloAeronaveRepository;
+	
+	@GetMapping("/modelos")
+	public ArrayList<ModeloAeronave> listarModelosAeronave() {
+		return (ArrayList<ModeloAeronave>) modeloAeronaveRepository.findAll();
+	}
+	
 }

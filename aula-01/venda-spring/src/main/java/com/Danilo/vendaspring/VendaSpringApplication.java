8f33f0f901f2adc78a0ccd1afb5faea215a82d4d
danilo775 <danilo.uba@hotmail.com>
2024-03-12 23:57:30 -0300

    Criaçao projeto venda

---
+++ aula-01/venda-spring/src/main/java/com/Danilo/vendaspring/VendaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.Danilo.vendaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendaSpringApplication.class, args);
+	}
+
+}
